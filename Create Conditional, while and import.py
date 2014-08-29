b1a0b6c7e600eba5d6b78baef14dbda0f6496ef6
pedro <pehecoro@hotmail.com>
2014-08-29 14:06:23 -0300

    AddNew

---
+++ Create Conditional, while and import.py
@@ -0,0 +1,27 @@
+#LearningPython
+#Author: Pedro
+#Function: Learn
+#Date: 29/08/2014
+
+#Primeiro importar de 'random' um inteiro 'randomico'
+from random import randint
+print("Bem vindo!")
+#Criar variável e atribui-la ao inteiro randomico determinando números de 1 a 100
+sorteado = randint(1, 100)
+#Atribuindo 0 para que a função while prossiga
+chute = 0
+#Enquanto o 'chute' for diferente do número sorteado, o programa repetirá
+while chute != sorteado:
+#Introduzir valor a variável 'g' / Entrada de dados.
+#Igualar variável 'g' com 'chute' e defini-la como número inteiro
+    g = input("Chute um número: ")
+    chute = int(g)
+#Possibilidades
+    if chute == sorteado:
+        print("Voce ganhou!")
+    else:
+        if chute > sorteado:
+            print("Alto")
+        else:
+            print("Baixo")
+print("Fim de jogo")
