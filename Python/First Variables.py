234f5442cb28e19622b43aa433e34a1d2d8bc492
Pedro <pehecoro@hotmail.com>
2014-08-29 14:28:10 -0300

    Create First Variables.py
    First step

---
+++ Python/First Variables.py
@@ -0,0 +1,12 @@
+#LearningPython
+#Author: Pedro
+#Function: Learn
+#Date: 2014/08/26
+#Language: Portuguese
+
+#Atribuir valores às variáveis
+a = 1
+b = 3
+
+#Exibir resultado da operação com variáveis declaradas
+print(a+b)
